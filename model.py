89b44cb738219d6a10b49ab37854c9eb47d98f3b
CRosenauer <christopherrosenauer101@gmail.com>
2022-11-16 21:04:00 -0800

    Fixed proper activation functions after convolutions

--- model.py
+++ model.py
@@ -6,93 +6,111 @@ import cv2
 import numpy
 
 
-lmbda = 3000
+class ClipSimNormalize(tf.keras.layers.Layer):
+    def __init__(self):
+        super(ClipSimNormalize, self).__init__()
+
+    def call(self, x):
+        # needs to mirror Quantize as the model must function with float32 outputs due to the supported args of the loss
+        # function. functionally it just simulates de-normalizing and clipping, but reverts the data to float os loss
+        # measurement can be performed properly.
+        result = tf.clip_by_value(x, 0.0, 1.0)
+        result = result * 255.0
+        result = tf.round(result)
+        result = result / 255.0
+
+        def grad(dy):
+            return tf.ones(shape=dy.shape)
+
+        return result
 
 
-def pass_through_loss(_, x):
-    return x
+class Quantize(tf.keras.layers.Layer):
+    def __init__(self):
+        super(Quantize, self).__init__()
 
+    def call(self, x):
+        result = tf.clip_by_value(x, 0.0, 1.0)
+        result = result * 255.0
+        result = tf.round(result)
+        result = result / 255.0
 
-def pass_through_distortion_approx(_, x):
-    return 1
+        def grad(dy):
+            return dy
+
+        return result
+
 
 def create_model():
     # encoder
     encoder_input = tf.keras.Input(shape=(512, 512, 3,), dtype=tf.float32, name="img_in")
 
-    # normalization
-    # x = tf.cast(encoder_input, dtype=tf.float32)
-    # x = x / 255.0
-
     x = tf.pad(encoder_input, [[0, 0], [2, 2], [2, 2], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=64, kernel_size=[5, 5], strides=2, input_shape=x.shape, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=64, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[5, 5], strides=2, input_shape=x.shape, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
 
     skip1 = x
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
 
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
 
     x = tf.keras.layers.add([x, skip1])
 
     skip2 = x
 
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, activation='relu')(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
 
     x = tf.keras.layers.add([x, skip2])
 
     x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], 'REFLECT')
-    encoder_output = tf.keras.layers.Conv2D(filters=96, kernel_size=[5, 5], strides=2, input_shape=x.shape,
-                                            activation='relu')(x)
+    encoder_output = tf.keras.layers.Conv2D(filters=96, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
 
     # quantize
+    # decoder_input = Quantize()(encoder_output)
+    decoder_input = encoder_output
 
     # decoder
     sub_pixel_factor = 2
 
     # sub-pixel conv 1
-    x = tf.keras.layers.Conv2D(filters=512, kernel_size=[3, 3], input_shape=encoder_output.shape, padding="same",
-                               activation='relu')(encoder_output)
+    x = tf.keras.layers.Conv2D(filters=512, kernel_size=[3, 3], input_shape=decoder_input.shape, padding="same")(decoder_input)
     x = tf.nn.depth_to_space(x, sub_pixel_factor)
     skip3 = x
 
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same", activation='relu')(
-        x)
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same", activation='relu')(
-        x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
 
     x = tf.keras.layers.add([x, skip3])
     skip4 = x
 
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same", activation='relu')(
-        x)
-    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same", activation='relu')(
-        x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
     x = tf.keras.layers.add([x, skip4])
 
     # sub-pixel conv 2
-    x = tf.keras.layers.Conv2D(filters=256, kernel_size=[3, 3], input_shape=x.shape, padding="same", activation='relu')(
-        x)
+    x = tf.keras.layers.Conv2D(filters=256, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.nn.depth_to_space(x, sub_pixel_factor)
 
     # sub-pixel conv 3
-    x = tf.keras.layers.Conv2D(filters=12, kernel_size=[3, 3], input_shape=x.shape, padding="same", activation='relu')(
-        x)
-    decoder_output = tf.nn.depth_to_space(x, sub_pixel_factor)
-
-    # denormalize
-    # x = x * 255.0
+    x = tf.keras.layers.Conv2D(filters=12, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
+    x = tf.nn.depth_to_space(x, sub_pixel_factor)
 
     # clip
-    # x = tf.clip_by_value(x, 0.0, 255.0)
-    # x = tf.math.floor(x)
-    # decoder_output = tf.cast(x, dtype=tf.float32)
+    # decoder_output = ClipSimNormalize()(x)
+    decoder_output = x
 
     autoencoder = tf.keras.Model(inputs=encoder_input, outputs=decoder_output, name="CAE_model")
     return autoencoder
@@ -106,12 +124,9 @@ if __name__ == "__main__":
             metrics=['accuracy']
             )
 
-    data_dir = os.path.dirname("data/130k")
-    batch_size = 1000
-
     # need to trip data. not enough ram
     X_data = []
-    files = glob.glob("./data/130k/artwork/*.jpeg", recursive=True)
+    files = glob.glob("./data/130k/toys/*.jpeg", recursive=True)
     for myFile in files:
         image = cv2.imread(myFile)
         X_data.append(image)
@@ -125,5 +140,5 @@ if __name__ == "__main__":
 
     # CAE.load_weights(checkpoint_path)
 
-    CAE.fit(X_data, X_data, epochs=32, shuffle=True, callbacks=[cp_callback])
+    CAE.fit(X_data, X_data, epochs=32, shuffle=True, callbacks=[cp_callback], validation_split=0.25)
     CAE.evaluate(X_data, X_data, callbacks=[cp_callback])
