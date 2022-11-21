aa242ca33c2410813a8c06fd558de17bd325ad74
CRosenauer <christopherrosenauer101@gmail.com>
2022-11-20 21:40:17 -0800

    finalized model. updated training code

--- model.py
+++ model.py
@@ -69,7 +69,7 @@ def create_model():
 
     x = tf.pad(encoder_input, [[0, 0], [2, 2], [2, 2], [0, 0]], 'REFLECT')
     x = tf.keras.layers.Conv2D(filters=64, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
-    # x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], 'REFLECT')
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
 
@@ -78,7 +78,7 @@ def create_model():
 
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
-    # x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
 
@@ -88,21 +88,17 @@ def create_model():
 
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
-    # x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.pad(x, [[0, 0], [1, 1], [1, 1], [0, 0]], 'REFLECT')
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape)(x)
 
     x = tf.keras.layers.add([x, skip2])
 
     x = tf.pad(x, [[0, 0], [2, 2], [2, 2], [0, 0]], 'REFLECT')
-    x = tf.keras.layers.Conv2D(filters=96, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
-    # x = tf.keras.activations.sigmoid(x)
-    # encoder_output = Round()(x)
-    encoder_output = x
+    encoder_output = tf.keras.layers.Conv2D(filters=96, kernel_size=[5, 5], strides=2, input_shape=x.shape)(x)
 
     # quantize
     decoder_input = QuantizeEmulation()(encoder_output)
-    # decoder_input = encoder_output
 
     # decoder
     sub_pixel_factor = 2
@@ -113,20 +109,20 @@ def create_model():
     skip3 = x
 
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
-    # x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
 
     x = tf.keras.layers.add([x, skip3])
     skip4 = x
 
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
-    # x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.keras.layers.Conv2D(filters=128, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
     x = tf.keras.layers.add([x, skip4])
 
     # sub-pixel conv 2
     x = tf.keras.layers.Conv2D(filters=256, kernel_size=[3, 3], input_shape=x.shape, padding="same")(x)
-    # x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.3)(x)
     x = tf.nn.depth_to_space(x, sub_pixel_factor)
 
     # sub-pixel conv 3
@@ -134,9 +130,6 @@ def create_model():
     x = tf.nn.depth_to_space(x, sub_pixel_factor)
 
     # clip
-    # decoder_output = ClipSimNormalize()(x)
-    # decoder_output = tf.keras.activations.sigmoid(x)
-    # decoder_output = Round()(x)
     decoder_output = QuantizeEmulation()(x)
 
     autoencoder = tf.keras.Model(inputs=encoder_input, outputs=decoder_output, name="CAE_model")
@@ -149,36 +142,28 @@ def create_model():
 if __name__ == "__main__":
     CAE = create_model()
     CAE.compile(
-            optimizer=tf.keras.optimizers.Adam(learning_rate=1e-5),
+            optimizer=tf.keras.optimizers.Adam(learning_rate=1e-6),
             loss=tf.keras.losses.MeanSquaredError(),
             metrics=['accuracy']
             )
 
-    files = glob.glob("./data/training/*.png", recursive=True)
+    files = glob.glob("./data/full/*/*.png", recursive=True)
 
-    n_files = len(files)
+    X_data = []
 
-    itr_size = 2000
-    n_itrs = math.ceil(n_files / itr_size)
+    for file in files:
+        image = cv2.imread(file)
+        X_data.append(image / 255.0)
 
-    for i in range(0, n_itrs):
-        # need to trip data. not enough ram
-        X_data = []
+    X_data = numpy.array(X_data, dtype='float32')
 
-        for j in range(i * itr_size, min(n_files, (i + 1) * itr_size)):
-            image = cv2.imread(files[j])
-            X_data.append(image / 255.0)
+    checkpoint_path = "training_1/cp.ckpt"
+    checkpoint_dir = os.path.dirname(checkpoint_path)
+    cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1, save_best_only=True)
 
-        X_data = numpy.array(X_data, dtype='float32')
+    # CAE.load_weights(checkpoint_path)
 
-        checkpoint_path = "training_1/cp.ckpt"
-        checkpoint_dir = os.path.dirname(checkpoint_path)
-        cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path, save_weights_only=True, verbose=1)
-
-        # CAE.load_weights(checkpoint_path)
-
-        CAE.fit(X_data, X_data, epochs=100000, shuffle=True, validation_split=0) # callbacks=[cp_callback],
-        break
+    CAE.fit(X_data, X_data, epochs=100, shuffle=True, validation_split=0.25, callbacks=[cp_callback])
 
     model_input = tf.constant(X_data[0], dtype=tf.dtypes.float32, shape=[128, 128, 3])
     model_output = CAE(X_data)
@@ -188,7 +173,5 @@ if __name__ == "__main__":
     model_output = model_output.astype('uint8')
 
     cv2.imwrite("output.png", model_output)
-    # im = PIL.Image.fromarray(model_output)
-    # im.save("output.png")
 
-    # CAE.evaluate(X_data, X_data)
+    CAE.evaluate(X_data, X_data)
