a4064acb20edcde32d9b454ca750ba05dbb33dbe
CRosenauer <christopherrosenauer101@gmail.com>
2022-11-29 12:39:37 -0800

    bug fixes

--- evaluate_model.py
+++ evaluate_model.py
@@ -11,21 +11,21 @@ import gc
 import math
 
 
-def Entropy(data):
+def Entropy(data, x_size, y_size, z_size):
     prob = dict()
 
-    size = 16*16*96
+    size = x_size*y_size*z_size
     inv_size = 1/size
 
-    for i in range(0, 16):
-        for j in range(0, 16):
-            for k in range(0, 96):
+    for i in range(0, x_size):
+        for j in range(0, y_size):
+            for k in range(0, z_size):
                 key = data[i, j, k]
                 prob[key] = 0.0
 
-    for i in range(0, 16):
-        for j in range(0, 16):
-            for k in range(0, 96):
+    for i in range(0, x_size):
+        for j in range(0, y_size):
+            for k in range(0, z_size):
                 key = data[i, j, k]
                 prob[key] = prob[key] + inv_size
 
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     X_data = []
 
-    eval_set_size = 250
+    eval_set_size = 13750
 
     random.seed(1024)  # constant seed to ensure same evaluation set between runs.
     used_data = random.sample(range(1, len(files)), eval_set_size)
@@ -64,11 +64,11 @@ if __name__ == "__main__":
 
     print("Beginning generating output files.")
 
-    num_batches = 13750
+    num_batches = 250
     batch_size = int(eval_set_size / num_batches)
 
     write_input_file_dir = r"F:\School\ENSC 424\ML-Project\validation\validation_input"
-    write_output_file_dir = r"F:\School\ENSC 424\ML-Project\validation\validation_output"
+    write_output_file_dir = r"F:\School\ENSC 424\ML-Project\validation\q_r128"
 
     for i in range(0, num_batches):
         start = i * batch_size
@@ -92,6 +92,7 @@ if __name__ == "__main__":
     encoded_output = CAE.layers[20].output
     encoder = tf.keras.models.Model(CAE_input, encoded_output)
 
+    input_entropy = 0.0
     total_entropy = 0.0
 
     print("Calculating average entropy of encoded images. This may take some time.")
@@ -106,15 +107,27 @@ if __name__ == "__main__":
         model_output = model_output.astype('uint8')
 
         for j in range(0, len(model_output)):
-            total_entropy = total_entropy + Entropy(model_output[j])
+            total_entropy = total_entropy + Entropy(model_output[j], 16, 16, 96)
 
-    average_entropy = total_entropy / eval_set_size
+
+        for j in range(0, len(data)):
+            input_entropy = input_entropy + Entropy(data[j], 128, 128, 3)
+
+    average_input_entropy = total_entropy / eval_set_size
+    average_entropy = input_entropy / eval_set_size
 
     print("Performing model evaluation")
     CAE.compile(loss=tf.keras.losses.MeanSquaredError(),
                 metrics=['accuracy'])
     CAE.evaluate(X_data, X_data)
 
+    print("Average Input Entropy: " + str(average_input_entropy))
     print("Average Output Entropy: " + str(average_entropy))
     print("Average Bits per Pixel: " + str(1.5 * average_entropy))
-    print("Average Compression Ratio: " + str((1.5 * average_entropy) / 24.0))
+
+    input_storage_size = 128*128*3*average_input_entropy
+    print("Average Input Storage Size: " + str(input_storage_size))
+    output_storage_size = 16*16*96*average_entropy
+    print("Average Encoded Storage Size: " + str(output_storage_size))
+
+    print("Average Compression: " + str(100 * output_storage_size / input_storage_size) + "%")
